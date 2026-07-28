860e21e751c58d0c125dae05040246ba9e15964a
pourya <pouryathesaymon@gmail.com>
2026-07-29 00:05:06 +0330

    validation added, refresh added

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -53,14 +53,30 @@ def read_user(
     credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],
     db: Session = Depends(get_db),
 ):
-    db_user = Crud.get_user_by_token(db, credentials.credentials)
+
+    access_token = credentials.credentials
+    try:
+        payload = jwt.decode(
+            access_token, config.SECRET_KEY, algorithms=[config.ALGORITHM]
+        )
+        username = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    db_user = Crud.get_user_by_token(db, access_token)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
 
 @router.post("/refresh", response_model=TokenResponse)
-def refresh_token(refresh_token: str, db: Session = Depends(get_db)):
+def refresh(
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],
+    db: Session = Depends(get_db),
+):
+    refresh_token = credentials.credentials
     try:
         payload = jwt.decode(
             refresh_token, config.SECRET_KEY, algorithms=[config.ALGORITHM]
